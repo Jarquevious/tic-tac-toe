7b41bdf0d16dbf1a0cfd62eff632c70d8565e04e
Jarquevious <jarquevious@gmail.com>
2021-03-29 11:53:01 -0700

    added tho.sprops.avalie to button

--- src/index.js
+++ src/index.js
@@ -5,18 +5,17 @@ import './index.css';
 
 class Square extends React.Component {
   render() {
-    return <button className="square">{/* TODO */}</button>;
+    return <button className="square">{this.props.value}</button>;
   }
 }
 
 class Board extends React.Component {
   renderSquare(i) {
-    return <Square />;
+    return <Square value={i}/>;
   }
 
   render() {
     const status = "Next player: X";
-
     return (
       <div>
         <div className="status">{status}</div>
