b3c7f50d00c18490fcd1e193937ae0e147eaba08
Jarquevious <jarquevious@gmail.com>
2021-04-02 02:02:57 -0700

    changed what happen to square when clicked

--- src/index.js
+++ src/index.js
@@ -15,7 +15,8 @@ class Square extends React.Component {
     return (
     <button 
     className="square" 
-    onClick={() => this.setState({value: 'X'})}>
+    onClick={() => this.props.onClick()}
+    >
       {this.state.value}
     </button>
     );
@@ -23,8 +24,17 @@ class Square extends React.Component {
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    }
+  }
   renderSquare(i) {
-    return <Square value={i}/>;
+    return <Square 
+    value={this.state.squares[i]}
+    onClick={() => this.handleClick(i)}
+    />;
   }
 
   render() {
